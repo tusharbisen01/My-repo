41075b1a710b7be8294ac1aafce773c4481cab77
tushar bisen <tusharbisen917215@gmail.com>
2025-09-09 16:34:46 +0530

    dockerimage

--- Desktop/PROJECTS/testapp/src/App.js
+++ Desktop/PROJECTS/testapp/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is the demo project of Docker!!!.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
